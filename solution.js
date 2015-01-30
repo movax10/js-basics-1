3fb05455a91343ecf0c370f930f51159c6b3f03c
ulitka <ulitka@37-115-31-173-broadband.kyivstar.net>
2015-01-31 00:17:20 +0200

    [Uliana Kucheriava] solution

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 function squaresOfMax(a, b, c) {
   // Write a function that takes three numbers and returns
   // sum of squares of two largest
+  if (a < b && a < c) {
+    a = 0;
+  }
+  else if (b < c) {
+    b = 0;
+  } else c = 0;
+  return a*a + b*b + c*c;
 }
 
 function drawTriangle() {
@@ -13,6 +20,11 @@ function drawTriangle() {
   // #####
   // ######
   // #######
+  var str="";
+  for (var n = 1; n <= 7; n++) {
+    str += '#';
+    console.log(str);
+  }
 }
 
 function fizzBuzz() {
@@ -23,6 +35,17 @@ function fizzBuzz() {
   // When you have that working, modify your program to print "FizzBuzz",
   // for numbers that are divisible by both 3 and 5 (and still print "Fizz"
   // or "Buzz" for numbers divisible by only one of those).
+  for (var n = 1; n <= 100; n++) {
+    var printedValue="";
+    if (n % 3 === 0){
+      printedValue += "Fizz";
+    }
+    if (n % 5 === 0){
+      printedValue += "Buzz";
+    }
+    printedValue = printedValue || n;
+    console.log(printedValue);
+  }
 }
 
 function chessBoard(size) {
@@ -45,6 +68,19 @@ function chessBoard(size) {
   // When you have a program that generates this pattern,
   // define a variable size = 8 and change the program so that
   // it works for any size, outputting a grid of the given width and height.
+  var even = "";
+  var odd = "";
+  var space = ' ';
+  var octothorpe = '#';
+  for (var i = 1; i <= size; i++){
+    even += (i%2 === 0) ? space : octothorpe;
+    odd  += (i%2 === 0) ? octothorpe : space;
+  }
+  var result = [];
+  for (var j = 1; j <= size; j++){
+    result.push((j%2 === 0) ? even : odd);
+  }
+  return result.join('\n');
 }
 
 module.exports.squaresOfMax = squaresOfMax;
